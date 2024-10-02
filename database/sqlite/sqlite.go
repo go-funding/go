836b9a9cd63f276130899a974a4c8401c80b2965
Gerasimovich Igor <mgorunuch.igor@gmail.com>
2024-10-02 12:28:42 +0100

    Add itterate rows

--- database/sqlite/sqlite.go
+++ database/sqlite/sqlite.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"fuk-funding/go/database"
 	_ "github.com/mattn/go-sqlite3"
+	"go.uber.org/multierr"
 )
 
 type Config struct {
@@ -26,6 +27,23 @@ type Database struct {
 	FilePath string
 
 	db *sql.DB
+}
+
+func (db *Database) IterateRows(ctx context.Context, query string, cb func(rows *sql.Rows) error, args ...any) (err error) {
+	rows, err := db.db.QueryContext(ctx, query, args...)
+	if err != nil {
+		return err
+	}
+
+	defer multierr.AppendInvoke(&err, multierr.Close(rows))
+	for rows.Next() {
+		err = cb(rows)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 func (db *Database) ExecContext(ctx context.Context, query string, args ...any) (sql.Result, error) {
